829aa24701f18f730686ce3f7a77c708539ec370
roviraz <alerovmar@alu.edu.gva.es>
2026-02-10 18:19:51 +0100

    seguir php mailer

--- Back/newPasswordRecovery.php
+++ Back/newPasswordRecovery.php
@@ -12,17 +12,24 @@
 <body>
     <div class="container align-items-center d-flex justify-content-center" style="height: 100vh;">
         <form class="form-horizontal row gap-3 text-end border border-dark border-5 rounded-5 p-2" action="recoveryData.php" method="POST">
-            <h2 class="text-center">recovery account form</h2>
+            <h2 class="text-center">recovery password form</h2>
 
             <div class="form-group col-12 row">
-                <label class="control-label col-4" for="email">Email:</label>
+                <label class="control-label col-4" for="password">Password:</label>
                 <div class="col-8">
-                    <input type="email" class="form-control" name="email" required>
+                    <input type="password" class="form-control" name="password" required>
+                </div>
+            </div>
+
+                <div class="form-group col-12 row">
+                <label class="control-label col-4" for="confirm_password">Confirm Password:</label>
+                <div class="col-8">
+                    <input type="password" class="form-control" name="confirm_password" required>
                 </div>
             </div>
 
             <div class="d-flex justify-content-center">
-                <button class="btn btn-dark mt-2" type="submit" name="enviar">SEND EMAIL</button>
+                <button class="btn btn-dark mt-2" type="submit" name="enviar">CHANGE PASSWORD</button>
             </div>
         </form>
     </div>
