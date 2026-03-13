1280f9f366c2fc85d75eff2bafc57d69e92b9a71
Angel <angruigar@alu.edu.gva.es>
2026-03-13 09:27:14 +0100

    Header responsive

--- recursos/php/header.php
+++ recursos/php/header.php
@@ -12,39 +12,44 @@ class Header
     public function toHTML()
     {
         return "
-            <header class=\"navbar luckiestGuyFontFamily\">
-                <nav class=\"container-fluid text-center\">
-                    <ul class=\"d-flex gap-3 navbar-nav flex-row align-items-center\">
-                        <li class=\"nav-item\">
-                            <a class=\"navbar-brand\" href=\"{$this->dots}/index.php \">
-                                <img src=\"{$this->dots}/recursos/img/VEX_logo.png\" alt=\"\" class=\"img-fluid mb-3\">
-                            </a>
-                        </li>
-                        <li class=\"nav-item\">
-                            <a class=\"nav-link active border border-5 border-black rounded-pill bg-white p-2\" aria-current=\"page\" href=\"{$this->dots}/Back/gamesPage.php \">Browse Games</a>
-                        </li>
-                        <li class=\"nav-item\">
-                            <a class=\"nav-link border border-5 border-black rounded-pill bg-white p-2\" aria-current=\"page\" href=\"{$this->dots}/Back/formGameRegister.php \">Upload Game</a>
-                        </li>
-                    </ul>
-                    <!-- ELIMINAR LOS BOTONES SI ESTA INICIADA LA SESION Y PONER UN BOTON PARA CERRARLA-->
-                    <ul class=\"d-flex gap-3 text-light align-items-center navbar-nav flex-row\">
-                        <li class=\"nav-item\">
-                            <a class=\"nav-link border border-5 border-black rounded-pill bg-white p-2\" aria-current=\"page\" href=\"{$this->dots}/# \">Login</a>
-                        </li>
-                        <li class=\"nav-item\">
-                            <a class=\"nav-link border border-5 border-black rounded-pill bg-white p-2\" aria-current=\"page\" href=\"{$this->dots}/# \">Register</a>
-                        </li>
-                        <li class=\"nav-item\" hidden>
-                            <a class=\"nav-link border border-5 border-black rounded-pill bg-white p-2\" aria-current=\"page\" href=\"{$this->dots}/# \">Log Out</a>
-                        </li>
-                        <li class=\"nav-item\">
-                            <a class=\"navbar-brand\" href=\"#\">
-                                <img src=\"{$this->dots}/recursos/img/sinperfil.webp\" alt=\"\" class=\"img-fluid rounded-circle border border-black border-3\" style=\"width: 8rem; height: 8rem;\">
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
+            <header class=\"navbar navbar-expand-lg luckiestGuyFontFamily\">
+                <div class=\"container-fluid\">
+                    <a class=\"navbar-brand\" href=\"{$this->dots}/index.php\">
+                        <img src=\"{$this->dots}/recursos/img/VEX_logo.png\" alt=\"\" class=\"img-fluid mb-3\">
+                    </a>
+                    <button class=\"navbar-toggler\" type=\"button\" data-bs-toggle=\"collapse\" data-bs-target=\"#navbarSupportedContent\" aria-controls=\"navbarSupportedContent\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">
+                        <span class=\"navbar-toggler-icon\"></span>
+                    </button>
+
+                    <div class=\"collapse navbar-collapse\" id=\"navbarSupportedContent\">
+                        <ul class=\"navbar-nav me-auto mb-2 mb-lg-0 gap-2\">
+                            <li class=\"nav-item\">
+                                <a class=\"nav-link active border border-5 border-black rounded-pill bg-white p-2\" aria-current=\"page\" href=\"{$this->dots}/Back/gamesPage.php\">Browse Games</a>
+                            </li>
+                            <li class=\"nav-item\">
+                                <a class=\"nav-link border border-5 border-black rounded-pill bg-white p-2\" aria-current=\"page\" href=\"{$this->dots}/Back/formGameRegister.php\">Upload Game</a>
+                            </li>
+                        </ul>
+
+                        <!-- ELIMINAR LOS BOTONES SI ESTA INICIADA LA SESION Y PONER UN BOTON PARA CERRARLA -->
+                        <ul class=\"navbar-nav ms-auto mb-2 mb-lg-0 gap-2 align-items-center\">
+                            <li class=\"nav-item\">
+                                <a class=\"nav-link border border-5 border-black rounded-pill bg-white p-2\" aria-current=\"page\" href=\"{$this->dots}/#\">Login</a>
+                            </li>
+                            <li class=\"nav-item\">
+                                <a class=\"nav-link border border-5 border-black rounded-pill bg-white p-2\" aria-current=\"page\" href=\"{$this->dots}/#\">Register</a>
+                            </li>
+                            <li class=\"nav-item\" hidden>
+                                <a class=\"nav-link border border-5 border-black rounded-pill bg-white p-2\" aria-current=\"page\" href=\"{$this->dots}/#\">Log Out</a>
+                            </li>
+                            <li class=\"nav-item\">
+                                <a class=\"navbar-brand\" href=\"#\">
+                                    <img src=\"{$this->dots}/recursos/img/sinperfil.webp\" alt=\"\" class=\"img-fluid rounded-circle border border-black border-3\" style=\"width: 8rem; height: 8rem;\">
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </header>
         ";
     }
